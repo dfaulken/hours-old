2308a1cf8c9a7a42c2bbab59a735c4ec8722701e
David Faulkenberry <david.faulkenberry@gmail.com>
2017-05-10 17:14:11 -0400

    Commit new files too

---
+++ app/mailers/hours_mailer.rb
@@ -0,0 +1,12 @@
+class HoursMailer < ApplicationMailer
+  default from: 'david.faulkenberry@gmail.com'
+  default to: ['adam@umass.edu', 'betsyt@admin.umass.edu']
+  # Just because I'm paranoid about how good I am at code
+  default bcc: 'david.faulkenberry@gmail.com'
+
+  before_action { @shifts = params[:shifts] }
+
+  def submit_timesheet
+    mail subject: "Timesheet #{params[:start_date]} to #{params[:end_date]}"
+  end
+end
