a8ebbeafa542a85b24a2f129866447989b895a20
David Faulkenberry <david.faulkenberry@gmail.com>
2017-05-10 20:43:09 -0400

    Secrets
    Just need to figure out how to access them in the controller. HTTP basic auth is good enough for me.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  binding.pry
+  http_basic_authenticate_with name: 'dave',
+                               password: 'secret'
   protect_from_forgery with: :exception
 end
