692a61f49f467863d2e7a1a46d8e10cfbbd4c62b
David Faulkenberry <david.faulkenberry@gmail.com>
2017-05-19 16:40:04 -0400

    BCC should be an array

--- app/mailers/hours_mailer.rb
+++ app/mailers/hours_mailer.rb
@@ -2,7 +2,7 @@ class HoursMailer < ApplicationMailer
   default from: 'david.faulkenberry@gmail.com'
   default to: ['adam@umass.edu', 'betsyt@admin.umass.edu']
   # Just because I'm paranoid about how good I am at code
-  default bcc: 'david.faulkenberry@gmail.com'
+  default bcc: ['david.faulkenberry@gmail.com']
 
   before_action { @shifts = params[:shifts] }
 
