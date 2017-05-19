5d1153abb7ac4c1facfefa9bee802af175dc1e4c
David Faulkenberry <david.faulkenberry@gmail.com>
2017-05-19 16:55:03 -0400

    CC me, and use my domain

--- app/mailers/hours_mailer.rb
+++ app/mailers/hours_mailer.rb
@@ -1,8 +1,8 @@
 class HoursMailer < ApplicationMailer
-  default from: 'david.faulkenberry@gmail.com'
+  default from: 'mailer-noreply@hours.davidfaulkenberry.com'
   default to: ['adam@umass.edu', 'betsyt@admin.umass.edu']
   # Just because I'm paranoid about how good I am at code
-  default bcc: ['david.faulkenberry@gmail.com']
+  default cc: ['david.faulkenberry@gmail.com']
 
   before_action { @shifts = params[:shifts] }
 
