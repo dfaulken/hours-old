f3134c47a6d75f7b34909500571b5fcb9e194205
David Faulkenberry <david.faulkenberry@gmail.com>
2017-05-19 17:08:38 -0400

    Use instance variable

--- app/mailers/hours_mailer.rb
+++ app/mailers/hours_mailer.rb
@@ -6,9 +6,13 @@ class HoursMailer < ApplicationMailer
   # Just because I'm paranoid about how good I am at code
   default cc: ['david.faulkenberry@gmail.com']
 
-  before_action { @shifts = params[:shifts] }
+  before_action do
+    @shifts = params[:shifts]
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
+  end 
 
   def submit_timesheet
-    mail subject: "Timesheet #{params[:start_date]} to #{params[:end_date]}"
+    mail subject: "Timesheet #{@start_date} to #{@end_date}"
   end
 end
