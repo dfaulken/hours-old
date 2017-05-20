efb8cad49e7e92382b4612671b860fa7b1c4adfc
David Faulkenberry <david.faulkenberry@gmail.com>
2017-05-19 22:05:14 -0400

    Make the mailer masquerade as me

--- app/mailers/hours_mailer.rb
+++ app/mailers/hours_mailer.rb
@@ -1,7 +1,7 @@
 ApplicationMailer.delivery_method = :sendmail
 
 class HoursMailer < ApplicationMailer
-  default from: 'mailer-noreply@hours.davidfaulkenberry.com'
+  default from: 'David Faulkenberry <mailer-noreply@hours.davidfaulkenberry.com>'
   default to: ['adam@umass.edu', 'betsyt@admin.umass.edu']
   # Just because I'm paranoid about how good I am at code
   default cc: ['david.faulkenberry@gmail.com']
